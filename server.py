fdbc83078cc0f3bedcc8b3d25ec13929ad0a8e97
prerak <prerak.gupta@tum.de>
2021-03-03 02:06:18 +0100

    v2

--- server.py
+++ server.py
@@ -1,26 +1,79 @@
+import os
+import time
+import random
+# import ktrain
+
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
+DOMAIN_MAP = {"beer": "Beer", "visa": "Visa", "movies": "Movies"}
+INDEX_DIR_MAP = {"beer": "beer wali", "visa": "visa wali", "movies": "war"}
+
 @app.route('/chat')
 def chat():
-  domains = ["Beer", "Visa", "Movies"]
-  # app.logger.debug('A value for debugging')
-  # app.logger.warning('A warning occurred (%d apples)', 42)
-  # app.logger.error('An error occurred')
-  return render_template('index.html', domains=domains)
+  return render_template('index.html', domains=DOMAIN_MAP)
 
 
 @app.route('/response', methods=["POST"])
-def query_response():
-  print(request.form)
+@cross_origin()
+def find_response():
+  params = request.get_json()
+  print(params)
   return({
-    "data": "Finally it works",
-    "modal_id": 12,
+    "data": query_qa(params),
+    "modal_id": generate_random_id(),
     "success": True
     })
+
+
+
+def formulate_query(params):
+  query = preprocess_query(params['query'])
+  print("PRE", query)
+
+  if params['use_context']=="1" and type(params['context'])==dict:
+    context_string = " for "
+    for key, value in params['context'].items():
+      context_string += str(key) + " " + str(value) + " and "
+    
+    query = query + context_string[0:-5] + " ?"
+    print("context string", context_string)
+    return query
+  else:
+    return query
+
+def preprocess_query(query):
+  if query[-1]!="?":
+    return query
+  else:
+    return query[0:-1]
+
+
+def process_answers(answers, params):
+  return [
+            {"Answer": "This is answer", "Context": "This is my full context", "Confidence": 0.95},
+            {"Answer": "answer", "Context": "This is my another full context", "Confidence": 0.90},
+            {"Answer": "answer", "Context": "This is my another full context", "Confidence": 0.80}
+          ]
+
+def query_qa(params):
+  index_dir = INDEX_DIR_MAP[params['domain']]
+  if index_dir:
+    # qa      = text.SimpleQA(index_dir)
+    query   = formulate_query(params)
+    print("Query: ", query)
+    # answers = qa.ask(query)
+    print("Index dir: ", index_dir)
+    return process_answers(answers = [], params = params)
+  else:
+    return []
+
+def generate_random_id():
+  return ( str(int(time.time())) + str(int(random.uniform(1, 100))) )
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,160 @@
-$(document).ready(function(){
-  //insert code here
-  
-});
+  function getFormData(){
+    var form = $("#all_settings")
+    var unindexed_array = form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function(n, i){
+        indexed_array[n['name']] = n['value'];
+    });
+
+    indexed_array['query'] = $("#input_query")[0].value;
+
+    try {
+      parsed_context = indexed_array['context'] ? JSON.parse(indexed_array['context']) : {}
+      indexed_array['context'] = parsed_context;
+      console.log(indexed_array);
+      return indexed_array;
+    }
+    catch (error) {
+      if (error instanceof SyntaxError) {
+          showError("There is a syntax error. Please correct it and try again: " + error.message);
+          return ;
+      }
+      else {
+          showError("An unexpected error occured.");
+          throw error;
+      }
+    }
+  }
+
+  function addUserQueryToDiv(query){
+    $(".chat_area").append("<div class='user_message'><img src='/static/images/user.png' alt='User Avatar' class='user_avatar'><p class='message'>"+query+"</p></div>");
+  }
+
+  function addBotResponseToDiv(modalId, answer){
+    BotResponseDiv = "<div class='bot_message'>";
+    BotResponseDiv += "<img src='/static/images/bot.jpg' alt='Bot Avatar' class='bot_avatar'>";
+    BotResponseDiv += "<p class='message'>"+answer+"</p>";
+    if(answer!=="Sorry I could not find anything relevant."){
+      BotResponseDiv += "<span class='show_more'><a href='#' class='small_message' onclick='showMore("+modalId+")'>Show more</a></span>";
+    }
+    BotResponseDiv += "</div>";
+    $(".chat_area").append(BotResponseDiv);
+    $(".chat_area").scrollTop($(".chat_area")[0].scrollHeight);
+  }
+
+  function getActiveIdAndResponse(resp){
+    if(resp.data && resp.data.length>1){
+      var modal_id = resp.modal_id;
+      var answer = resp.data[0].Answer;
+    }
+    else{
+      var modal_id = Date.now();
+      var answer = "Sorry I could not find anything relevant."
+    }
+    return [modal_id, answer];
+  }
+
+  function showMore(el){
+    $("#"+el).show();
+  }
+
+  function showError(error_message){
+    $("#error_div").append("<p class='error'>"+error_message+"</p>")
+    $("#error_div").show()
+    setTimeout(function(){
+      $("#error_div").hide()
+      $("#error_div").html('')
+    }, 4000) 
+  }
+
+  function onQueryChange(event){
+    var key = window.event.keyCode;
+    if (key === 13) {
+      var user_query = $("#input_query")[0].value;
+      if (user_query.length > 5){
+        $(".chat_area")[0].innerHTML = "";
+        addUserQueryToDiv(user_query);
+
+        
+        $.ajax({
+          type: "POST",
+          url: "http://localhost:5000/response",
+          contentType: "application/json",
+          dataType: "json",
+          headers: {
+            "accept": "application/json",
+            "Access-Control-Allow-Headers": "Content-Type"
+          },
+          data: JSON.stringify(getFormData()),
+          success: function(response){
+            console.log(response);
+            if(response.success && response.data){
+              [modalId, answer] = getActiveIdAndResponse(response);
+              $("#input_query")[0].value = '';
+              addBotResponseToDiv(modalId, answer);
+              addMoreInfoPopup(response, modalId);
+            }
+            else{
+              showError(response.message);
+            }
+          },
+          error: function(response){
+            showError("An error has occured. Make sure you have selected a domain.")
+          }
+        });
+      }
+      else{
+        showError("The query is too small to process.");
+      }
+    }
+  }
+
+
+  function addMoreInfoPopup(response, modalId){
+    popupWindowHTML = "<div class='modal_class' id="+modalId+" style='display: none;'>";
+    popupWindowHTML += "<div class='modal_content'><table><tr><th>No.</th><th>Answer</th><th>Confidence</th></tr>";
+    console.log(response.data);
+    jQuery.each(response.data, function(index, resp) {
+      popupWindowHTML += "<tr><td>"+index+"</td><td>"+resp.Answer+"</td><td>"+resp.Confidence+"</td></tr>";
+    });
+    popupWindowHTML += "<div class='popup_close_button' onclick='closePopup()'>X</div></div></div>"
+    $("#more_info")[0].innerHTML += popupWindowHTML;
+  }
+
+  function closePopup(){
+    $(".modal_class").hide();
+  }
+
+  function reset(){
+    $(".chat_area")[0].innerHTML = "";
+    $("#more_info")[0].innerHTML="";
+    $(".info-box").show();
+  }
+
+  $("#show_settings").on('click', function(){
+    $("#show_settings").hide();
+    $("#all_settings").show();
+    $("#hide_settings").show();
+  });
+
+  $("#hide_settings").on('click', function(){
+    $("#hide_settings").hide();
+    $("#all_settings").hide();
+    $("#show_settings").show();
+  });
+
+  $(".nav_domains").on('click', function(el){
+    var domain = el.currentTarget.getAttribute("href").substr(1, )
+    $("#current_domain")[0].value = domain;
+    var li_elements = el.currentTarget.parentElement.parentElement.getElementsByTagName("li");
+    jQuery.each(li_elements, function(index, li) {
+      li.getElementsByTagName("a")[0].style = "color: white";
+    });
+    el.currentTarget.style.color="#5294e2";
+    reset();
+  });
+
+
+
+
